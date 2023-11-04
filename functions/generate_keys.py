97edf0c1dbbd79ad61ab4240c585df5b5ac8d6db
shaulleo <99772221+shaulleo@users.noreply.github.com>
2023-11-04 23:26:40 +0000

    merge pfv

--- functions/generate_keys.py
+++ functions/generate_keys.py
@@ -12,3 +12,5 @@ hashed_passwords = stauth.Hasher(passwords).generate()
 file_path = Path(__file__).parent.parent / "data" / "hashed_pw.pkl"
 with file_path.open("wb") as file:
     pickle.dump(hashed_passwords, file)
+
+#testesssss.
