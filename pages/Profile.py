b5d53d405a2092204262f6d054af31a1922668f1
shaulleo <99772221+shaulleo@users.noreply.github.com>
2023-11-05 20:32:24 +0000

    colocar mudanças boozis

--- pages/Profile.py
+++ pages/Profile.py
@@ -3,30 +3,76 @@ import pandas as pd
 from functions.streamlitfunc import *
 
 
+user_data_df = pd.DataFrame(columns=[
+    "Email", "First Name", "Last Name", "Gender", "Date of Birth", "Nationality",
+    "City", "Has Travel Car", "Drinks Alcohol", "Dietary Restrictions",
+    "Allergies", "Favourite Food", "Dislike Food", "Personality", "Preferred Payment",
+    "Restaurant Style", "Cuisine Type", "Lunch Hour", "Dinner Hour", "Normal Price Range",
+    "Smoker"
+])
+#save as a csv file the user_data_df
+user_data_df.to_csv("user_data.csv")
+
+
 # email - temos que arranjar forma de se o utilizador já tiver feito login o email aparecer automaticamente
 # e arranjar forma de caso o mail já exista e a pessoa fizer novamente fazer replace dos inputs
-email = st.text_input("Email")
-first_name = st.text_input("First Name")
-last_name = st.text_input("Last Name")
-gender = st.selectbox("Gender", ["Male", "Female", "Other"])
-date_of_birth = st.date_input("Date of Birth")
-nationality = st.text_input("Nationality")
-city = st.text_input("City")
-has_travel_car = st.checkbox("Do you have a travel car?")
-drinks_alcohol = st.checkbox("Do you drink alcohol?")
-dietary_restrictions = st.multiselect("Dietary Restrictions", ["Vegetarian", "Vegan", "Gluten-Free", "Dairy-Free"])
-allergies = st.text_area("Allergies")
-favourite_food = st.text_area("Favourite Food")
-dislike_food = st.text_area("Dislike Food")
-personality = st.text_area("Personality")
-preferred_payment = st.selectbox("Preferred Payment", ["Credit Card", "Cash", "Mobile Payment"])
-restaurant_style = st.multiselect("Restaurant Style", ["Casual", "Fine Dining", "Fast Food", "Cafe"])
-cuisine_type = st.multiselect("Cuisine Type", ["Italian", "Asian", "American", "Mediterranean"])
-lunch_hour = st.slider("Lunch Hour", min_value=11, max_value=15,value=(11, 15))
-dinner_hour = st.slider("Dinner Hour", min_value=18, max_value=22, value=(18, 22))
-normal_price_range = st.slider("Normal Price Range", min_value=0, max_value=100, value=(0, 50))
-smoker = st.checkbox("Are you a smoker?")
+def client_data():
+    
+   
+    
+    first_name = st.text_input("First Name")
+    last_name = st.text_input("Last Name")
+    email = st.text_input("Email")
+    gender = st.selectbox("Gender", ["Male", "Female", "Other"])
+    date_of_birth = st.date_input("Date of Birth")
+    nationality = st.text_input("Nationality")
+    city = st.text_input("City")
+    has_travel_car = st.checkbox("Do you have a travel car?")
+    drinks_alcohol = st.checkbox("Do you drink alcohol?")
+    dietary_restrictions = st.multiselect("Dietary Restrictions", ["Vegetarian", "Vegan", "Gluten-Free", "Dairy-Free"])
+    allergies = st.text_area("Allergies")
+    favourite_food = st.text_area("Favourite Food")
+    dislike_food = st.text_area("Dislike Food")
+    personality = st.text_area("Personality")
+    preferred_payment = st.selectbox("Preferred Payment", ["Credit Card", "Cash", "Mobile Payment"])
+    restaurant_style = st.multiselect("Restaurant Style", ["Casual", "Fine Dining", "Fast Food", "Cafe"])
+    normal_price_range = st.slider("Normal Price Range", min_value=0, max_value=100, value=(0, 50))
+    cuisine_type = st.multiselect("Cuisine Type", ["Italian", "Asian", "American", "Mediterranean"])
+    lunch_hour = st.slider("Lunch Hour", min_value=11, max_value=15,value=(11, 15))
+    dinner_hour = st.slider("Dinner Hour", min_value=18, max_value=22, value=(18, 22))
+    smoker = st.checkbox("Are you a smoker?")
 
 
 
-st.button(label="Discover Your Personality :)", on_click=lambda: nav_page("Personality"))
+    if st.button(label="Discover Your Personality :)"):
+        global user_data_df
+        new_data = pd.DataFrame({
+            "Email": email,
+            "First Name": [first_name],
+            "Last Name": [last_name],
+            "Gender": [gender],
+            "Date of Birth": [date_of_birth],
+            "Nationality": [nationality],
+            "City": [city],
+            "Has Travel Car": [has_travel_car],
+            "Drinks Alcohol": [drinks_alcohol],
+            "Dietary Restrictions": [dietary_restrictions],
+            "Allergies": [allergies],
+            "Favourite Food": [favourite_food],
+            "Dislike Food": [dislike_food],
+            "Personality": [personality],
+            "Preferred Payment": [preferred_payment],
+            "Restaurant Style": [restaurant_style],
+            "Cuisine Type": [cuisine_type],
+            "Lunch Hour": [lunch_hour],
+            "Dinner Hour": [dinner_hour],
+            "Normal Price Range": [normal_price_range],
+            "Smoker": [smoker]
+        })
+
+        # Concatenate the new data with the existing user_data_df
+        user_data_df = pd.concat([user_data_df, new_data], ignore_index=True)
+        
+        nav_page("Personality")
+
+client_data()
--- pages/Restaurant.py
+++ pages/Restaurant.py
@@ -1,18 +1,97 @@
+
 import streamlit as st
 import pandas as pd
 from functions.streamlitfunc import *
+from streamlit_extras.metric_cards import style_metric_cards
+from streamlit_extras.stylable_container import stylable_container
 
-data = pd.read_csv('/Users/madalena.frango/Desktop/capstone/FlavourFlix/data/all_thefork_scrapes.csv')
+
+
+st.set_page_config(page_title='Restaurant', page_icon="logo.jpeg", layout="wide", initial_sidebar_state="collapsed")
+data = pd.read_csv('/Users/madalena.frango/Desktop/capstone/FlavourFlix/data/preprocessed_data.csv')
+st.title("Restaurant's Details")
 
 def restaurant_details():
-    st.markdown(f"## {selected_restaurant}")
-    col1, col2 = st.columns([3,3])
+
+    st.markdown(f"<h1 style='text-align: center; color: black;'>{selected_restaurant}</h1>", unsafe_allow_html=True)
+
+    st.markdown('<br>', unsafe_allow_html=True)
+    m1, m2, m3, m4 = st.columns([1,1,1, 1])
+    with m1:
+        with stylable_container(
+        key="container_with_border",
+        css_styles="""
+            {
+                border: 1px solid rgba(49, 51, 63, 0.2);
+                border-radius: 0.5rem;
+                padding: calc(1em - 1px)
+            }
+            """,
+    ):
+            st.metric(label = 'Food Rating', value = data.loc[data['name'] == selected_restaurant, 'foodRatingSummary'].iloc[0])
+    with m2:
+        with stylable_container(
+        key="container_with_border",
+        css_styles="""
+            {
+                border: 1px solid rgba(49, 51, 63, 0.2);
+                border-radius: 0.5rem;
+                padding: calc(1em - 1px)
+            }
+            """,
+    ):
+            st.metric(label = 'Service Rating', value = data.loc[data['name'] == selected_restaurant, 'serviceRatingSummary'].iloc[0])
+    with m3:
+        with stylable_container(
+        key="container_with_border",
+        css_styles="""
+            {
+                border: 1px solid rgba(49, 51, 63, 0.2);
+                border-radius: 0.5rem;
+                padding: calc(1em - 1px)
+            }
+            """,
+    ):
+            st.metric(label = 'Max Party Size', value = data.loc[data['name'] == selected_restaurant, 'maxPartySize'].iloc[0])
+    with m4:
+        with stylable_container(
+        key="container_with_border",
+        css_styles="""
+            {
+                border: 1px solid rgba(49, 51, 63, 0.2);
+                border-radius: 0.5rem;
+                padding: calc(1em - 1px)
+                align-items: center;
+            }
+            """,
+    ):
+            st.metric(label= 'Michelin?', value=data.loc[data['name'] == selected_restaurant,'michelin'].iloc[0])
+    # style_metric_cards(border_left_color='#FFFFFF', box_shadow=False)
+    st.markdown('<br>', unsafe_allow_html=True)
+    st.markdown('<br>', unsafe_allow_html=True)
+    col1, col2 = st.columns([3,3], gap = 'medium')
     with col1:
         st.image(data.loc[data['name'] == selected_restaurant, 'photo'].iloc[0])
     with col2:
-        st.markdown(f"**Address:** {data.loc[data['name'] == selected_restaurant, 'address'].iloc[0]}")
-        st.markdown(f"**Phone:** {data.loc[data['name'] == selected_restaurant, 'phone'].iloc[0]}")
-        st.markdown(f"**Cuisine:** {data.loc[data['name'] == selected_restaurant, 'cuisine'].iloc[0]}")
+        with stylable_container(
+        key="container_with_border",
+        css_styles="""
+            {
+                border: 1px solid rgba(49, 51, 63, 0.2);
+                border-radius: 0.5rem;
+                padding: calc(1em - 1px)
+                text-align: left
+            }
+            """,
+    ):
+            st.markdown(f"**Address:** {data.loc[data['name'] == selected_restaurant, 'address'].iloc[0]}")
+            st.markdown(f"**Phone:** {data.loc[data['name'] == selected_restaurant, 'phone'].iloc[0]}")
+            st.markdown(f"**Cuisine:** {data.loc[data['name'] == selected_restaurant, 'cuisine'].iloc[0]}")
+            st.markdown(f"**Style:** {data.loc[data['name'] == selected_restaurant,'style'].iloc[0]}")
+            if st.button(f"Reserve Now!"):
+                st.session_state.selected_restaurant = selected_restaurant
+                nav_page("Reservations")
+
 
 if 'selected_restaurant' not in st.session_state:
     selected_restaurant = st.session_state.selected_restaurant = None
@@ -21,20 +100,24 @@ else:
 
     
 
-# If the user has not selected a restaurant yet, show a selectbox
+    # If the user has not selected a restaurant yet, show a selectbox
 if selected_restaurant is None:
     # Get a list of all the restaurants
-    restaurants = data['name'].unique().tolist()
-        # Add an option for no selection
-    restaurants = [""] + restaurants
-        # Create a selectbox for the user to choose a restaurant
-    selected_restaurant = st.selectbox("Select Restaurant", restaurants)
-        # Store the selected restaurant in session state
-    st.session_state.selected_restaurant = selected_restaurant
+    restaurants = ["Search"] + data['name'].unique().tolist()
+    # Create a selectbox for the user to choose a restaurant
+    selected = st.selectbox("Select Restaurant", restaurants)
+    if selected != "Search":
+        selected_restaurant = data.loc[data['name'] == selected, 'name'].iloc[0]
+    # Store the selected restaurant in session state
+        st.session_state.selected_restaurant = selected_restaurant
+        restaurant_details()
+    else:
+        st.write("Please select a restaurant from the list above :)")
+
+
     
 else:
     # select in the select box the restaurant that was selected before
-    selected_restaurant = st.selectbox("Select Restaurant", [selected_restaurant])
+    #selected_restaurant = st.selectbox("Select Restaurant", [selected_restaurant])
     restaurant_details()
-
 
